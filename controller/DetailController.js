6a1ef8cff132835a8c61f606a4c72dedff720b1e
Kyxie <kyriexie@outlook.com>
2022-02-24 20:39:35 -0500

    username

--- controller/DetailController.js
+++ controller/DetailController.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-01-18 16:30:41
  * @LastEditors: Kunyang Xie
- * @LastEditTime: 2022-02-24 20:29:12
+ * @LastEditTime: 2022-02-24 20:39:12
  * @FilePath: \backend\controller\DetailController.js
  */
 
