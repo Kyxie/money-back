dacca25f0a130310aa04236a38d4de913bf7c05f
Steven <shaowei_sun@outlook.com>
2022-03-02 15:52:06 -0500

    new vision

--- controller/ChartController.js
+++ controller/ChartController.js
@@ -1,12 +1,7 @@
 /*
  * @Date: 2022-02-28 23:17:42
-<<<<<<< HEAD
  * @LastEditors: Shaowei Sun
- * @LastEditTime: 2022-03-02 14:40:39
-=======
- * @LastEditors: Kunyang Xie
- * @LastEditTime: 2022-03-02 15:39:00
->>>>>>> 92c866168d4bdcefcc57173b8360e17ae2d3af4f
+ * @LastEditTime: 2022-03-02 15:51:19
  * @FilePath: \Money_Back\controller\ChartController.js
  */
 
